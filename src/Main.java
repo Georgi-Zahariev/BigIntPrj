293b47b854a03c867135b7a0ca4bb668e8884eed
GEORGI ZAHARIEV <GEORGI ZAHARIEV@DESKTOP-MRDPU1L>
2021-03-10 11:51:57 +0200

    another

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
  public static void main(String[] args) {
-  BigInt b=new BigInt("999999999999322321"),c=new BigInt("223231");
+  BigInt b=new BigInt("999999999"),c=new BigInt("1");
   System.out.println(b.add(c));
  }
 }
